2a6cd2715b52816abc0dc97f853587259b3d5679
EstebanPastrana <esteban.pastrana@ulatina.net>
2024-11-21 00:49:03 -0600

    Epastrana, se agregaron eventos a los botones reiniciar y enviar, se mejoro la logica del script

--- scripts.js
+++ scripts.js
@@ -1,16 +1,43 @@
 const timerElement = document.getElementById("timer");
-let totalTime = 30;
+
+let selectdPreguntaSuma;
+let selectdPreguntaCorrecta;
+let selectdPreguntaCielo;
+
+let date = new Date();
+let isfinish = false
 
 function beginTimer(){
-    let time = totalTime
-    const interval = setInterval(function(){
-        if(time > 0){
-            time--;
-            timerElement.textContent =time.toString();
-        }
-        else{
-            alert("El tiempo se ha terminado");
-        }
-    }, 1000);
+    let time = 30
+
+    if(!isfinish){
+            setInterval(function(){
+            if(time > 0){
+                time--;
+                timerElement.textContent =time.toString();
+            }
+            else{
+                finish()
+            }
+        }, 1000);
+    }
 }
+
+function checkQuestions(nameQuestion){
+    selectdQuestion = document.querySelector(`input[name="${nameQuestion}"]:checked`);
+    return selectdQuestion ? selectdQuestion.value : "No se respondio"
+}
+
+function finish(){
+    isfinish = true
+
+    alert(` Fecha: ${date.toLocaleDateString('es-Es')}
+            La respuesta de la pregunta 1: ${checkQuestions("preguntaSuma")}
+            La respuesta de la pregunta 2: ${checkQuestions("preguntaCielo")}
+            La respuesta de la pregunta 3: ${checkQuestions("preguntaCorrecta")}
+        `
+    );
+    location.reload();
+}
+
 
